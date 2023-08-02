d50720cd7cafae22d8d7cfb91d8650494469d8ed
Tomas Aranda <tomas@devschool.com.ar>
2023-08-02 15:35:01 -0300

    comments

---
+++ src/context/AuthContext.js
@@ -0,0 +1,16 @@
+import { useState, createContext } from 'react';
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+	// usuario estatico (de momento no existe)
+	const [user, setUser] = useState(null);
+
+	// los datos para utilizar en todo el sitio web
+	const data = {
+		user,
+		setUser,
+	};
+
+	// el contexto
+	return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
+};
