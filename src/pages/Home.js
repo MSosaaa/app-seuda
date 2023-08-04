e691cb1bbdd4faa5bff86caa14c1ac89a60c5fb9
Tomas Aranda <tomas@devschool.com.ar>
2023-08-04 17:41:31 -0300

    comments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,20 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+
 const Home = () => {
 	const { user } = useContext(AuthContext);
 
-	return (
+	return user ? (
 		<>
-			<div className="mx-2">
+			<div className="mx-2 d-flex  flex-column align-items-center">
 				<h3>
 					Home - Bienvenido{' '}
 					<span className="text-primary">{user.firstname + ' ' + user.lastname}</span>
 				</h3>
+				<button className="btn btn-danger w-50 mt-3">Cerrar Sesion</button>
 			</div>
 		</>
-	);
+	) : null;
 };
 
 export default Home;
